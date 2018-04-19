a4904ef0579b12d81fb6f0ca789cc053d762f856
胡浩 <qwhuhaopp520@163.com>
2018-04-19 16:56:58 +0800

    First save!

---
+++ JenkinsTeTests/JenkinsTeTests.swift
@@ -0,0 +1,36 @@
+//
+//  JenkinsTeTests.swift
+//  JenkinsTeTests
+//
+//  Created by 胡胡浩 on 2018/4/19.
+//  Copyright © 2018年 胡胡浩. All rights reserved.
+//
+
+import XCTest
+@testable import JenkinsTe
+
+class JenkinsTeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
